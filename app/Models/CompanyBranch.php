9150a152f9df70674bdb0a28c627ba1c5a31e3f0
James Jilhaney <notail2111@gmail.com>
2023-08-24 12:52:30 +0800

    added company branch fillable property and softdeletes

--- app/Models/CompanyBranch.php
+++ app/Models/CompanyBranch.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CompanyBranch extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'branch_name',
+        'branch_address',
+        'branch_coordinates',
+    ];
 }
